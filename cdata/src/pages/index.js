ea974bd8c57a82402bafdf03be0754b0cf8da273
sha7hid <shahidshafadshahad@gmail.com>
2023-02-18 20:45:25 +0530

    h5

--- cdata/src/pages/index.js
+++ cdata/src/pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
       </Navbar>
           <div>
             <h1 className={styles.title}>Welcome to cdata</h1>
-            <h4 className={styles.title}>Details Of Students and Teachers are available here</h4>
+            <h5 className={styles.title}>Details Of Students and Teachers are available here</h5>
           
           </div>
         </main>
